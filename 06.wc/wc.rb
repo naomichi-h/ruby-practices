1b2a3cb0de2d8cf274de9fa77401dd4b1ab45e15
naomichi-h <naomichi.programming@gmail.com>
2021-03-28 18:23:41 +0900

    rubocop-fjordに通しました

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,13 +1,14 @@
-require "optparse"
+#!/usr/bin/env ruby
+# frozen_string_literal: true
 
+require 'optparse'
 
 def main(options)
-
-  if ARGV[0] == nil
+  if ARGV[0].nil?
     text = $stdin.read
     print text.count("\n").to_s.rjust(8)
     print text.split(/\s+/).size.to_s.rjust(8)
-    print text.bytesize.to_s.rjust(8) + "\n"
+    print "#{text.bytesize.to_s.rjust(8)}\n"
   else
 
     files = ARGV
@@ -17,57 +18,54 @@ def main(options)
     else
       word_count(files)
     end
+  end
 end
 
-end
-
-#wcコマンド（オプションなし）
+# wcコマンド（オプションなし）
 def word_count(files)
-total_lines = 0
-total_words = 0
-total_bytesize = 0
+  total_lines = 0
+  total_words = 0
+  total_bytesize = 0
 
+  files.each do |file|
+    text = File.read(file)
+    print text.count("\n").to_s.rjust(8)
+    print text.split(/\s+/).size.to_s.rjust(8)
+    print text.bytesize.to_s.rjust(8)
+    puts " #{file}"
 
-files.each do |file|
-  text = File.read(file)
-  print text.count("\n").to_s.rjust(8)
-  print text.split(/\s+/).size.to_s.rjust(8)
-  print text.bytesize.to_s.rjust(8)
-  puts " #{file}"
+    total_lines += text.count("\n")
+    total_words += text.split(/\s+/).size
+    total_bytesize += text.bytesize
+  end
 
-  total_lines += text.count("\n")
-  total_words += text.split(/\s+/).size
-  total_bytesize += text.bytesize
-end
+  # ファイルが複数ある場合はtotalを表示する
+  return unless files[1]
 
-#ファイルが複数ある場合はtotalを表示する
-if files[1]
   print total_lines.to_s.rjust(8)
   print total_words.to_s.rjust(8)
   print total_bytesize.to_s.rjust(8)
-  puts " total"
-end
+  puts ' total'
 end
 
-#wcコマンド（lオプション）
+# wcコマンド（lオプション）
 def word_count_l(files)
   total_lines = 0
-    
+
   files.each do |file|
     text = File.read(file)
     print text.count("\n").to_s.rjust(8)
     puts " #{file}"
-  
+
     total_lines += text.count("\n")
   end
-  
-  #ファイルが複数ある場合はtotalを表示する
-  if files[1]
-    print total_lines.to_s.rjust(8)
-    puts " total"
-  end
-  end
-  
+
+  # ファイルが複数ある場合はtotalを表示する
+  return unless files[1]
+
+  print total_lines.to_s.rjust(8)
+  puts ' total'
+end
 
 options = ARGV.getopts('l')
 main(options)
